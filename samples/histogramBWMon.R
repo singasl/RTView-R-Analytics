1dd9550b8c18a728ae1b8a4ce42fd35a5642cea4
singasl <singasl@users.noreply.github.com>
2016-03-18 12:06:20 -0700

    Update histogramBWMon.R

--- samples/histogramBWMon.R
+++ samples/histogramBWMon.R
@@ -1,8 +1,8 @@
 # Set your working directory
-setwd("C:/Users/m/Desktop/SL/GitHub/Repositories/RTView-R-Analytics/")
+setwd("C:/Users/GitHub/Repositories/RTView-R-Analytics/")
 
 # Read data from your BWMon dataserver
-bw <- read.delim("http://192.168.200.105:8068/bwmon_rtvquery/cache/HostStats/current?fmt=text")
+bw <- read.delim("http://myhost:myport/bwmon_rtvquery/cache/HostStats/current?fmt=text")
 dim(bw)
 
 # List column names in HostStats cache
